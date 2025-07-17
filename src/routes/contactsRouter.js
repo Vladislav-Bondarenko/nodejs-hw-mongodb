fa1b1e910debcaa9cbecb2bd392997532bc2326f
Vladislav Bondarenko <bondarenkovladislav2018@gmail.com>
2025-07-17 14:17:59 +0300

    5 Tasks done

--- src/routes/contactsRouter.js
+++ src/routes/contactsRouter.js
@@ -8,16 +8,28 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contactsSchemas.js';
+
 const router = express.Router();
 
 router.get('/', getAllContactsController);
 
-router.get('/:contactId', getContactByIdController);
+router.get('/:contactId', isValidId, getContactByIdController);
 
-router.post('/', createContactController);
+router.post('/', validateBody(createContactSchema), createContactController);
 
-router.patch('/:contactId', updateContactController);
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  updateContactController,
+);
 
-router.delete('/:contactId', deleteContactController);
+router.delete('/:contactId', isValidId, deleteContactController);
 
 export default router;
